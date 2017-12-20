f14f6834942025316b1be437e846e78a231688bd
anshuman singh <anshuman singh>
2017-12-20 13:59:15 +0530

    improvement over password

--- src/app/components/user-create/user-create.component.ts
+++ src/app/components/user-create/user-create.component.ts
@@ -11,14 +11,61 @@ import {Router} from "@angular/router";
 })
 export class UserCreateComponent implements OnInit {
 
-  user={password:'agriledger',role:'farmer'} as Iuser;
-  passwordRetyped:string='agriledger';
+  user={password:'',role:'farmer'} as Iuser;
+  passwordRetyped:string='';
   roles=['ops','sponsor','farmer'];
     createRequestStatus='resolved';
 
     constructor(private userService:UserService,private toastService:ToastService,private router:Router) { }
 
   ngOnInit() {
+        this.createPassword();
+  }
+
+  createPassword(){
+      const specials = '!@&*';
+      const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+      const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      const numbers = '0123456789';
+
+      const all = specials + lowercase + uppercase + numbers;
+
+      String.prototype.pick = function(min, max) {
+          let n, chars = '';
+
+          if (typeof max === 'undefined') {
+              n = min;
+          } else {
+              n = min + Math.floor(Math.random() * (max - min));
+          }
+
+          for (let i = 0; i < n; i++) {
+              chars += this.charAt(Math.floor(Math.random() * this.length));
+          }
+
+          return chars;
+      };
+
+
+// Credit to @Christoph: http://stackoverflow.com/a/962890/464744
+      String.prototype.shuffle = function() {
+          let array = this.split('');
+          let tmp, current, top = array.length;
+
+          if (top) while (--top) {
+              current = Math.floor(Math.random() * (top + 1));
+              tmp = array[current];
+              array[current] = array[top];
+              array[top] = tmp;
+          }
+
+          return array.join('');
+      };
+
+      let password = (specials.pick(2) + lowercase.pick(4) + uppercase.pick(2)).shuffle();
+      this.user.password=password;
+      this.passwordRetyped=password;
+
   }
     register(){
 
@@ -45,6 +92,7 @@ export class UserCreateComponent implements OnInit {
             this.toastService.success('User','created successfully');
             this.user.name=null;
             this.user.email=null;
+            this.createPassword();
 
         },(err)=>{
             this.createRequestStatus='rejected';
